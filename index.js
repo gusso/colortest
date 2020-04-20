278ca88477dc80cb6e75405ebf63559cb743e6b8
Henrique Gusso <henrique@gus.so>
2020-04-20 17:04:02 +0100

    Remove unused files

--- index.js
+++ index.js
@@ -5,27 +5,6 @@ StyleDictionary.registerFilter({
   matcher: (prop) => prop.attributes.type !== "base" && !prop.hideInDesign,
 })
 
-const go = {
-  platforms: {
-    json: {
-      transforms: [
-        // The following transforms map the property name, attribute
-        // and color values into formats the plugin expects.
-        "name/ti/camel",
-        "attribute/cti",
-        "color/sketch",
-      ],
-      files: [
-        {
-          // This defines the file format of the output.
-          format: "json",
-          destination: "output.json",
-        },
-      ],
-    },
-  },
-}
-
 StyleDictionary.extend({
   source: ["properties/color/*.json"],
   platforms: {
@@ -34,7 +13,7 @@ StyleDictionary.extend({
       files: [
         {
           format: "json",
-          destination: "variables.json",
+          destination: "build/variables.json",
           filter: "filter",
         },
       ],
